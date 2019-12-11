947793918fb8afa27e4fbc44ce473e52cd199fd6
Olli Korhonen <olli.korhonen19@gmail.com>
2019-12-11 19:55:20 +0200

    Updates

---
+++ precache-manifest.88de7a83e9e5196ec2b5d68d3aa6f9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d4e2b0ae2e292c8dfde83961de32c2",
+    "url": "/GithubApiExample/index.html"
+  },
+  {
+    "revision": "a8554c5a7d631e7eb417",
+    "url": "/GithubApiExample/static/css/main.84209e3e.chunk.css"
+  },
+  {
+    "revision": "0c88e128fcc2e59cf4a9",
+    "url": "/GithubApiExample/static/js/2.253ded25.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/GithubApiExample/static/js/2.253ded25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8554c5a7d631e7eb417",
+    "url": "/GithubApiExample/static/js/main.795bb267.chunk.js"
+  },
+  {
+    "revision": "57c11152322bf5d01131",
+    "url": "/GithubApiExample/static/js/runtime-main.33332a47.js"
+  }
+]);
